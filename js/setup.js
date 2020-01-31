623a58988422486134f598d6d4ee8c0646296236
audiopie <surroundyou@ya.ru>
2020-01-31 14:07:00 +0300

    add similar wizards characters

---
+++ js/setup.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var setup = document.querySelector('.setup');
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var WIZARDS_COUNT = 4;
+var WIZARDS_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var WIZARDS_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var EYE_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+
+// Функция, для случайного выбора из списка элементов для персонажа
+var getRandom = function (array) {
+  var randomElement = Math.floor(Math.random() * array.length);
+  return randomElement;
+};
+
+// Функция, для создания массива состоящий из 4-х сгенерированных JS объектов, которые будут описывать похожих персонажей.
+var makeArrayWizards = function () {
+  var wizardsArray = [];
+
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    var wizards = {};
+    wizardsArray[i] = wizards;
+    wizardsArray[i].name = WIZARDS_NAMES[getRandom(WIZARDS_NAMES)];
+    wizardsArray[i].surname = WIZARDS_SURNAME[getRandom(WIZARDS_SURNAME)];
+    wizardsArray[i].coatColor = COAT_COLOR[getRandom(COAT_COLOR)];
+    wizardsArray[i].eyeColor = EYE_COLOR[getRandom(EYE_COLOR)];
+  }
+  return wizardsArray;
+};
+
+// Функция для изменения характеристик персонажа
+var renderWizard = function (wizard, nodeElement) {
+  nodeElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
+  nodeElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  nodeElement.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
+  return nodeElement;
+};
+
+// Добавление в DOM
+var appendWizards = function (wizArr) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < WIZARDS_COUNT; i++) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
+    wizardElement = renderWizard(wizArr[i], wizardElement);
+    fragment.appendChild(wizardElement);
+  }
+  similarListElement.appendChild(fragment);
+};
+
+
+setup.classList.remove('hidden');
+setup.querySelector('.setup-similar').classList.remove('hidden');
+
+
+var getWizards = makeArrayWizards();
+appendWizards(getWizards);
